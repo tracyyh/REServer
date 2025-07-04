ea74d8ead804e3d121ce3641688e7f1eca34c7de
Sid Kulkarni <ksidarth16@gmail.com>
2025-07-04 11:26:57 +1000

    fixup

--- src/main/java/app/REServer2.java
+++ src/main/java/app/REServer2.java
@@ -3,7 +3,6 @@ package app;
 import io.javalin.Javalin;
 import io.javalin.openapi.plugin.OpenApiPlugin;
 import io.javalin.openapi.plugin.swagger.SwaggerPlugin;
-import myopenai.user.UserController;
 import sales.SalesDAO;
 import sales.SalesController;
 
@@ -14,11 +13,11 @@ import org.slf4j.LoggerFactory;
 
 public class REServer2 {
 
-    private static final Logger LOG = LoggerFactory.getLogger(REServer.class);
+    private static final Logger LOG = LoggerFactory.getLogger(REServer2.class);
 
     public static void main(String[] args) {
 
-        // In-memory test data store
+        // MongoDB-backed data store
         var sales = new SalesDAO();
 
         // API implementation
@@ -39,32 +38,48 @@ public class REServer2 {
 
             // API routes
             config.router.apiBuilder(() -> {
-                // path("users", () -> {
-                //     get(UserController::getAll);
-                //     post(UserController::create);
-                //     path("{userId}", () -> {
-                //         get(UserController::getOne);
-                //         patch(UserController::update);
-                //         delete(UserController::delete);
-                //     });
-                // });
+                // Sales records are immutable hence no PUT and DELETE
+
+                // return a sale by sale ID
+                get("/sales/{saleID}", ctx -> {
+                    salesHandler.getSaleByID(ctx, ctx.pathParam("saleID"));
+                });
+
+                get("/", ctx -> {
+                    ctx.result("Real Estate server is running");
+                });
+
+                // get all sales records - could be big!
+                get("/sales", ctx -> {
+                    salesHandler.getAllSales(ctx);
+                });
+
+                // create a new sales record
+                post("/sales", ctx -> {
+                    salesHandler.createSale(ctx);
+                });
+
+                // Get all sales for a specified postcode
+                get("/sales/postcode/{postcode}", ctx -> {
+                    salesHandler.findSaleByPostCode(ctx, ctx.pathParam("postcode"));
+                });
+
+                // Get all sales in a price range
+                get("/sales/price/{low}/{high}", ctx -> {
+                    int low = Integer.parseInt(ctx.pathParam("low"));
+                    int high = Integer.parseInt(ctx.pathParam("high"));
+                    ctx.json(sales.getSalesByPriceRange(low, high));
+                });
+
+                // Get the average sale price for a specified postcode
+                get("/sales/postcode/{postcode}/average", ctx -> {
+                    salesHandler.getAvgPriceByPostCode(ctx, ctx.pathParam("postcode"));
+                });
             });
+        });
 
-                // // Sales routes
-                // path("sales", () -> {
-                //     get(ctx -> salesHandler.getAllSales(ctx));
-                //     post(ctx -> salesHandler.createSale(ctx));
-
-                //     path("{saleID}", () -> {
-                //         get(ctx -> salesHandler.getSaleByID(ctx, ctx.pathParam("saleID")));
-                //     });
-
-                //     path("postcode/{postcode}", () -> {
-                //         get(ctx -> salesHandler.findSaleByPostCode(ctx, ctx.pathParam("postcode")));
-                //     });
-                // });
-            
-        }).start(7002);
+        // Start server
+        app.start(7002);
 
         LOG.info("Real Estate server started at http://localhost:7002");
         LOG.info("Swagger UI available at http://localhost:7002/swagger");
