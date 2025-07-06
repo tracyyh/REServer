2f3b9fd8af7002d4585a0962bd096ee62fa04e2d
Sid Kulkarni <ksidarth16@gmail.com>
2025-07-07 09:36:29 +1000

    SalesQuery

---
+++ src/main/java/sales/SalesQuery.java
@@ -0,0 +1,9 @@
+package sales;
+
+public class SalesQuery {
+    private int queryID;
+    private String queryType;
+    private String queryDatetime;
+    private String params;
+    private int status;
+}
