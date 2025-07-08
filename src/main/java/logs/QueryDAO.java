e38e780c447a1bf508f3eac799ee5382e37740a6
Sid Kulkarni <ksidarth16@gmail.com>
2025-07-08 11:38:14 +1000

    working version of postgresql

--- src/main/java/logs/QueryDAO.java
+++ src/main/java/logs/QueryDAO.java
@@ -1,9 +1,5 @@
 package logs;
 
-import org.bson.Document;
-import org.bson.conversions.Bson;
-import com.mongodb.client.MongoCollection;
-import static com.mongodb.client.model.Filters.eq;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,26 +9,18 @@ import java.sql.Statement;
 
 public class QueryDAO {
 
-   private MongoCollection<Document> collection;
-
+   private Connection connection;
 
    public QueryDAO() {
     try {
-        // Connect to Neon PostgreSQL database using JDBC
         String url = "jdbc:postgresql://ep-wild-cherry-a7rmik76-pooler.ap-southeast-2.aws.neon.tech/neondb?user=neondb_owner&password=npg_7MXptljGJe0D&sslmode=require&channelBinding=require";
         String user = "neondb_owner";
         String password = "npg_HavSVn2Zy6bi";
  
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+        try {
+            this.connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to Neon database!");
 
-            // Example query
-            Statement stmt = conn.createStatement();
-            String sqlStr = "SELECT * from sales LIMIT 100";
-            ResultSet rs = stmt.executeQuery(sqlStr);
-            while (rs.next()) {
-                System.out.println("Property ID " + rs.getString("property_id"));
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -42,21 +30,33 @@ public class QueryDAO {
         }
     }
 
-    public static void main (String[] args) {
-        QueryDAO dao = new QueryDAO();
-    }
-
-
     public int getPostcodeCount(int postcode) {
-        Bson query = eq("params", "post_code=" + Integer.toString(postcode));
-        long count = collection.countDocuments(query);
-        return (int) count;
+        String cond = "'postcode=" + postcode + "'";
+        String query = "SELECT COUNT(*) from sales_query where query_params = " + cond;
+        try (Statement stmt = this.connection.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1; 
     }
+        return 0;
+}
 
     public int getPropertyCount(int propertyId) {
-        Bson query = eq("params", "property_id=" + Integer.toString(propertyId));
-        long count = collection.countDocuments(query);
-
-        return (int) count;
+        String cond = "'postcode=" + propertyId + "'";
+        String query = "SELECT COUNT(*) FROM sales_query WHERE query_params = " + cond;
+        try (Statement stmt = this.connection.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return 0;
     }
 }
